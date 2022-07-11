077c4e998795e2002d203398c12c287da07a662c
sweta__7_ <sweta300498@yandex.ru>
2022-07-11 17:17:18 +0300

    правки

--- js/js.js
+++ js/js.js
@@ -3,10 +3,9 @@
     const header = document.querySelector(".header");
     window.onscroll = () => {
       if (window.pageYOffset > 0) {
-        decor.classList.add("displaynone");
         header.style.backgroundColor = '#ff9800';
       } else {
-        decor.classList.remove("displaynone");
+        header.style.backgroundColor = '#ff9800ba';
       }
     };
   })();
