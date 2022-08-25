cd8d4c2fe4028b1f11ef77f713e69e3761ad4f44
sweta__7_ <sweta300498@yandex.ru>
2022-08-25 12:43:51 +0300

    адаптив

--- js/js.js
+++ js/js.js
@@ -1,26 +1,36 @@
+// (function () {
+//     const decor = document.querySelector(".hiro__decor");
+//     const header = document.querySelector(".header");
+//     window.onscroll = () => {
+//       if (window.pageYOffset > 0) {
+//         // header.style.background = 'linear-gradient(to top, black, var(--bg-color))';
+//         header.style.background = 'rgb(0 0 0 / 99%)';
+//       } else {
+//         header.style.background = ' var(--bg-color)';
+//       }
+//     };
+//   })();
 (function () {
-    const decor = document.querySelector(".hiro__decor");
-    const header = document.querySelector(".header");
-    window.onscroll = () => {
-      if (window.pageYOffset > 0) {
-        header.style.backgroundColor = '#ff9800';
-      } else {
-        header.style.backgroundColor = '#ff9800ba';
-      }
-    };
-  })();
-  (function () {
-    const acsess = document.querySelector(".main__link-two");
-    const instrumental = document.querySelector(".main__link-one ");
-    const instrumentalCard = document.querySelector(".instrumental");
-    const acsessCard = document.querySelector(".acsess");
-    acsess.addEventListener("click", () => {
-        acsessCard.classList.remove("displaynone");
-        instrumentalCard.classList.add("displaynone");
-    });
-    instrumental.addEventListener("click", () => {
-        instrumentalCard.classList.remove("displaynone");
-        acsessCard.classList.add("displaynone");
+  const acsess = document.querySelector(".category__acces");
+  const instrumental = document.querySelector(".category__instrum ");
+  const instrumentalCard = document.querySelector(".instrumental");
+  const acsessCard = document.querySelector(".acsess");
+  acsess.addEventListener("click", () => {
+    acsessCard.classList.remove("displaynone");
+    instrumentalCard.classList.add("displaynone");
+  });
+  instrumental.addEventListener("click", () => {
+    instrumentalCard.classList.remove("displaynone");
+    acsessCard.classList.add("displaynone");
+  });
+})();
 
-    });
-  })();
+(function () {
+  if(document.querySelector(".map-click")){
+  const map = document.getElementById("map");
+  let click = document.querySelector('.map-click');
+  click.addEventListener("click", () => {
+  map.scrollIntoView({ behavior: "smooth" });
+
+  });}
+})();
