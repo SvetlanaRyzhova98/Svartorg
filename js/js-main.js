ec1c500db8b1a2177fdd494c9dc110fe43e2cc6b
sweta__7_ <sweta300498@yandex.ru>
2022-08-29 15:56:14 +0300

    верстка страниц статья услуги

---
+++ js/js-main.js
@@ -0,0 +1,22 @@
+(function () {
+    const acsess = document.querySelector(".category__acces");
+    const instrumental = document.querySelector(".category__instrum ");
+    const instrumentalCard = document.querySelector(".instrumental");
+    const acsessCard = document.querySelector(".acsess");
+    acsess.addEventListener("click", () => {
+      acsessCard.classList.remove("displaynone");
+      instrumentalCard.classList.add("displaynone");
+    });
+    instrumental.addEventListener("click", () => {
+      instrumentalCard.classList.remove("displaynone");
+      acsessCard.classList.add("displaynone");
+    });
+  })();
+
+  (function () {
+    const artclick = document.querySelector(".articles");
+    artclick.addEventListener("click", () => {
+      window.open("/article.html")
+    
+      });  
+    })();
--- js/js.js
+++ js/js.js
@@ -10,20 +10,7 @@
 //       }
 //     };
 //   })();
-(function () {
-  const acsess = document.querySelector(".category__acces");
-  const instrumental = document.querySelector(".category__instrum ");
-  const instrumentalCard = document.querySelector(".instrumental");
-  const acsessCard = document.querySelector(".acsess");
-  acsess.addEventListener("click", () => {
-    acsessCard.classList.remove("displaynone");
-    instrumentalCard.classList.add("displaynone");
-  });
-  instrumental.addEventListener("click", () => {
-    instrumentalCard.classList.remove("displaynone");
-    acsessCard.classList.add("displaynone");
-  });
-})();
+
 
 (function () {
   if(document.querySelector(".map-click")){
@@ -34,3 +21,5 @@
 
   });}
 })();
+
+
