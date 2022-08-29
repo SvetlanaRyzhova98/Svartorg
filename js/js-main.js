3a4c68c4d4b62d12df827fac498581505dc9f303
sweta__7_ <sweta300498@yandex.ru>
2022-08-29 18:53:37 +0300

    верстка

--- js/js-main.js
+++ js/js-main.js
@@ -20,3 +20,14 @@
     
       });  
     })();
+
+
+    (function () {
+        let select = document.querySelector(".location-select");
+        let locCity = document.querySelector(".location-city");
+        const arrow = document.querySelector(".fa-arrow-circle-down");
+        select.addEventListener("click", () => {
+            locCity.classList.toggle("visible");
+            arrow.classList.toggle("rotate180")
+        })
+    })();
